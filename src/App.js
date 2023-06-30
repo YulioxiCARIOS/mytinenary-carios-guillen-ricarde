cf0a57f47b25971e680724a9c552e271fb1e93ff
ZoraliaGuillen <zguillena@gmail.com>
2023-06-30 19:58:00 -0300

    add CityDetail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './style/App.css';
 import Home from './component/Home';
 import Cities from './component/Cities';
+import CityDetail from './component/CityDetail';
 import { Routes, Route } from "react-router-dom"
 
 //-----LOGICA------
@@ -14,6 +15,7 @@ function App() {// FUNCION DONDE HABITA TODO EL CONTENIDO
         <Route path="*" element={<Home />}></Route>
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/Cities" element={<Cities />}></Route>
+        <Route path="/CityDetail" element={<CityDetail />}></Route>
       </Routes>
     </div>
   );
--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -1,38 +1,54 @@
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
+import "../style/App.css"
 import NavBar from "../component/NavBar";
-import axios from "axios";
+//import axios from "axios";
 import Footerr from "./Footer";
-//import CitiesPrueba from "./CitiesPrueba"
 import { FaExclamationTriangle } from "react-icons/fa";
 
 function Cities() {
-    const [cities, setCities] = useState([])
-    let citiesDb
-    async function getData() {
-        citiesDb = await axios.get("http://localhost:4000/api/cities")
-        setCities(citiesDb.data.response.cities)
-        
-        console.log(citiesDb)
-    }
-    useEffect(() => {
-        getData()
-  
-    },[])
-    useEffect(() => {
-        console.log(cities)
-    },[cities])
+    // const [cities, setCities] = useState([])
+    // let citiesDb
+    // async function getData() {
+    //     //citiesDb = await axios.get("http://localhost:4000/api/cities")
+    //     //setCities(citiesDb.data.response.cities)
+    //     //console.log(citiesDb)
+    // }
+    // useEffect(() => {
+    //     getData()
+
+    // }, [])
+    // useEffect(() => {
+    //     console.log(cities)
+    // }, [cities])
+    
+    const navigate = useNavigate();
+
+    const clickCities = () => {
+        navigate('/CityDetail'); // Ruta a la que se redirigirá al hacer clic en el botón
+    };
+
     return (
         <>
             <NavBar />
             <div className="cities">
-                <FaExclamationTriangle className="exclamation-triangle" />
+                <div className="card-content">
+                    <div className="card">
+                        <div className='image-cities'></div>
+                        <div className="text">
+                            <h2>Buenos Aires</h2>
+                            <p>Argentina</p>
+                        </div>
+                        <div className="text-info">
+                            <p><i className="fa-solid fa-money-bill-wave"></i>Price: $</p>
+                            <button className="btn-cities" onClick={clickCities}>Ver Detalles</button>
+                        </div>
+                    </div>
+                </div>
+                {/* <FaExclamationTriangle className="exclamation-triangle" />
                 <h1>PAGE</h1>
-                <h3>UNDER CONSTRUCTION</h3>
+                <h3>UNDER CONSTRUCTION</h3> */}
             </div>
-
-            {/* <CitiesPrueba /> */}
-
-
             <Footerr />
         </>
     )
