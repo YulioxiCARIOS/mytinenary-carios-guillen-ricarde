bb78300bbc75c94cd69d3a167f7d98a2f810eb26
YulioxiCARIOS <cariosyulioxi@gmail.com>
2023-06-30 14:26:30 -0300

    Enlace de cities con el backend

--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import NavBar from "../component/NavBar";
+import axios from "axios";
 import Footerr from "./Footer";
 //import CitiesPrueba from "./CitiesPrueba"
 import { FaExclamationTriangle } from "react-icons/fa";
 
 function Cities() {
+    const [cities, setCities] = useState([])
+    let citiesDb
+    async function getData() {
+        citiesDb = await axios.get("http://localhost:4000/api/cities")
+        setCities(citiesDb.data.response.cities)
+        
+        console.log(citiesDb)
+    }
+    useEffect(() => {
+        getData()
+  
+    },[])
+    useEffect(() => {
+        console.log(cities)
+    },[cities])
     return (
         <>
             <NavBar />
