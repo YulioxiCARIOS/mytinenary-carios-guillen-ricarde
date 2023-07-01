838a332c2578ef82d3ab024d600c7855b555031e
YulioxiCARIOS <cariosyulioxi@gmail.com>
2023-07-01 11:50:08 -0300

    Inf de cards dinamica

--- src/component/Cities.jsx
+++ src/component/Cities.jsx
@@ -1,27 +1,26 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
-import "../style/App.css"
 import NavBar from "../component/NavBar";
-//import axios from "axios";
+import axios from "axios";
 import Footerr from "./Footer";
-import { FaExclamationTriangle } from "react-icons/fa";
+
 
 function Cities() {
-    // const [cities, setCities] = useState([])
-    // let citiesDb
-    // async function getData() {
-    //     //citiesDb = await axios.get("http://localhost:4000/api/cities")
-    //     //setCities(citiesDb.data.response.cities)
-    //     //console.log(citiesDb)
-    // }
-    // useEffect(() => {
-    //     getData()
+    const [cities, setCities] = useState([])
+    let citiesDb
+    async function getData() {
+        citiesDb = await axios.get("http://localhost:4000/api/cities")
+        setCities(citiesDb.data.response.cities)
+        //console.log(citiesDb)
+    }
+    useEffect(() => {
+        getData()
 
-    // }, [])
-    // useEffect(() => {
-    //     console.log(cities)
-    // }, [cities])
-    
+    }, [])
+    useEffect(() => {
+        console.log(cities)
+    }, [cities])
+
     const navigate = useNavigate();
 
     const clickCities = () => {
@@ -30,25 +29,40 @@ function Cities() {
 
     return (
         <>
+
+
+
             <NavBar />
+
+
+
+
+
             <div className="cities">
-                <div className="card-content">
-                    <div className="card">
-                        <div className='image-cities'></div>
-                        <div className="text">
-                            <h2>Buenos Aires</h2>
-                            <p>Argentina</p>
-                        </div>
-                        <div className="text-info">
-                            <p><i className="fa-solid fa-money-bill-wave"></i>Price: $</p>
-                            <button className="btn-cities" onClick={clickCities}>Ver Detalles</button>
-                        </div>
+                {cities.length > 0 ? (
+                    <div>
+                        {cities.map((city) => (
+                            <div className="card-content" key={city.id}>
+                                <div className="card">
+                                    <div className='image-cities'> <img src={city.image} alt="" /> </div>
+                                    <div className="text">
+                                        <h2>{city.name}</h2>
+                                        <p>{city.country}</p>
+                                    </div>
+                                    <div className="text-info">
+                                        <p><i className="fa-solid fa-money-bill-wave"></i>Price: $</p>
+                                        <button className="btn-cities" onClick={clickCities}>Ver Detalles</button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                </div>
-                {/* <FaExclamationTriangle className="exclamation-triangle" />
-                <h1>PAGE</h1>
-                <h3>UNDER CONSTRUCTION</h3> */}
+                ) : <h1>Loading...</h1>}
             </div>
+
+
+
+
             <Footerr />
         </>
     )
